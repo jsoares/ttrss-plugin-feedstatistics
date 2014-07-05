d352af34d82b3d5941c10162843b6d9129576413
jsoares <mail@jorgesoares.org>
2014-07-05 14:46:14 +0200

    Adding initial code and updating README.

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 ttrss-plugin-feedstatistics
 ===========================
 
-Simple feed statistics for Tiny Tiny RSS
+Basic feed statistics for Tiny Tiny RSS. 
+
+Adds a statistics pane to the feed preferences tab, including both a Google Reader-style one line summary, as well as a table of statistics per feed.
+
+Notes
+-----
+
+1) The table is sorted by posts/day. No way to change this in the preferences, but you can easily change it in the SQL query.
+
+2) Unlike Google Reader, we do not keep track of actual feed activity, but instead only look at the current database. This has a few implications,
+e.g. we do not keep lifetime read counts. If you don't have purging enabled, you can still get generate them by modifying the default interval (30 days). 
+
+3) The items per day calculation assumes all feeds were followed for the entire interval. If they were recently added, the value will be wrong.
+
+Installation
+------------
+
+1) Upload the feedstatistics directory into [TT-RSS root directory]/plugins.
+2) Set the correct permissions, if neeeded.
+3) Enable the plugin by going to Preferences -> Plugins.
+
+Changelog
+---------
+
+v1.0 - Initial Release (2014-07-05)
+
+Links
+-----
+
+For discussion, please see the following thread on the Tiny Tiny RSS forums.
+TODO
---
+++ feedstatistics/init.php
@@ -0,0 +1,75 @@
+<?php
+class FeedStatistics extends Plugin {
+
+	function about() {
+		return array(1.0,
+			"Provides simple statistics on your feeds",
+			"jsoares",
+			false,
+			"");
+	}
+
+	function init($host) {
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
+	}
+
+	function hook_prefs_tab($args) {
+		if ($args != "prefFeeds") return;
+
+		print "<div dojoType=\"dijit.layout.AccordionPane\" title=\"".__("Statistics")."\">"; # start pane
+		
+		$owner_uid = $_SESSION["uid"] ? $_SESSION["uid"] : "NULL";
+		
+		// By default, use previous 30 days for statistics. 
+		$interval = 30;
+		// However, if the purge limit is lower, adjust accordingly
+		$result = db_query("SELECT value FROM ttrss_user_prefs
+							WHERE pref_name = 'PURGE_OLD_DAYS' AND owner_uid = $owner_uid");
+		if (db_num_rows($result) == 1) {
+			$purge_limit = db_fetch_result($result, 0, "value");
+			$interval = min($interval,$purge_limit);
+		}
+		
+		// Google Reader-like one-line summary
+		$result = db_query("SELECT COUNT(DISTINCT feed_id) AS `Feeds`, COUNT(DISTINCT ref_id) as `Items`, SUM(marked) as `Starred`, SUM(published) AS `Published`
+							FROM ttrss_user_entries 				
+							WHERE last_read > DATE_SUB(CURDATE(),INTERVAL {$interval} DAY)
+							AND owner_uid = {$owner_uid}");
+		if(db_num_rows($result)) {		
+			$row = db_fetch_assoc($result);
+			print_notice("From your " . $row['Feeds'] . " subscriptions, over the last {$interval} days you read " . $row['Posts'] . " items, starred " . $row['Starred'] . " items, and published " .  $row['Published'] . " items.");
+		}
+		
+		// Per-feed statistics
+		$result = db_query("SELECT ttrss_feeds.title as `Feed`, ttrss_feed_categories.title as `Category`, COUNT(ref_id) as `Items`, 
+							SUM(marked) as `Starred`, SUM(published) AS `Published`, ROUND(COUNT(ref_id)/{$interval},2) as `Items/day`
+							FROM ttrss_user_entries 
+							INNER JOIN ttrss_feeds ON ttrss_user_entries.feed_id = ttrss_feeds.id
+							INNER JOIN ttrss_entries ON ttrss_user_entries.ref_id = ttrss_entries.id
+							INNER JOIN ttrss_feed_categories ON ttrss_feeds.cat_id=ttrss_feed_categories.id							
+							WHERE ttrss_entries.date_entered > DATE_SUB(CURDATE(),INTERVAL {$interval} DAY)
+							AND ttrss_user_entries.owner_uid = {$owner_uid}
+							GROUP BY feed_id
+							ORDER BY `Items/day` DESC");
+		if(db_num_rows($result)) {
+			print "<table cellpadding=\"5\" class=\"feed-table\">";
+			print "<tr class=\"title\"><td>Feed</td><td>Category</td><td>Items</td><td>Starred</td><td>Published</td><td>Items/day</td></tr>";
+			while($row = db_fetch_assoc($result)) {
+				print "<tr>";
+				foreach($row as $key=>$value) {
+					print "<td>{$value}</td>";
+				}
+				print "</tr>";
+			}
+			print "</table>";
+		}		
+		
+		print "</div>"; #pane
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}
+?>
