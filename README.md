1916cc117ec59942ef0a330e9ef69be03dc8a7bd
jsoares <mail@jorgesoares.org>
2016-01-21 03:36:16 +0000

    Updated SQL so that marking and publishing dates are now taken into account.

--- README.md
+++ README.md
@@ -33,11 +33,13 @@ v1.01 - Made compatible with pgSQL (2014-07-05)
 
 v1.02 - Fixed bug introduced in 1.01 in items/day calculation for low purge delays (2014-07-06)
 
-v1.03 - Now also showing feeds with 0 posts in the period. Version changed to match expected format. (2014-07-08)
+v1.03 - Now also showing feeds with 0 posts in the period. Version changed to match expected format (2014-07-08)
 
 v1.04 - Replaced sums with counts (2014-07-08)
 
 v1.05 - Fixed bug when purge is disabled (2015-10-05)
+
+v1.06 - Instead of just looking at recently read items, it now looks at marking/publishing dates (2016-01-21)
 
 Links
 -----
--- feedstatistics/init.php
+++ feedstatistics/init.php
@@ -2,7 +2,7 @@
 class FeedStatistics extends Plugin {
 
 	function about() {
-		return array(1.03,
+		return array(1.06,
 			"Provides simple statistics on your feeds",
 			"jsoares",
 			false,
@@ -36,22 +36,31 @@ class FeedStatistics extends Plugin {
 		$datestr = $date->format("Y-m-d");
 		
 		// Google Reader-like one-line summary
-		$result = db_query("SELECT COUNT(DISTINCT ttrss_feeds.id) AS feeds, COUNT(DISTINCT ref_id) as items, COUNT(NULLIF(marked, false)) as starred, COUNT(NULLIF(published, false)) AS published
+		$result = db_query("SELECT
+							COUNT(DISTINCT ttrss_feeds.id) AS feeds,
+							COUNT(NULLIF(last_read > '{$datestr}', false)) AS items,
+							COUNT(NULLIF(last_marked > '{$datestr}', false)) AS starred,
+							COUNT(NULLIF(last_published > '{$datestr}', false)) AS published
 							FROM ttrss_feeds
-							LEFT JOIN ttrss_user_entries ON ttrss_feeds.id = ttrss_user_entries.feed_id AND last_read > '{$datestr}'
+							LEFT JOIN ttrss_user_entries ON ttrss_feeds.id = ttrss_user_entries.feed_id
 							WHERE ttrss_feeds.owner_uid = {$owner_uid}");
 		if(db_num_rows($result)) {		
 			$row = db_fetch_assoc($result);
-			print_notice("From your " . $row['feeds'] . " subscriptions, over the last {$interval} days you read " . $row['items'] . " items, starred " . $row['starred'] . " items, and published " .  $row['published'] . " items.");
+			print_notice("From your " . $row['feeds'] . " subscriptions, over the last {$interval} days  you read " . $row['items'] . " items, starred " . $row['starred'] . " items, and published " .  $row['published'] . " items.");
 		}
 		
 		// Per-feed statistics
-		$result = db_query("SELECT ttrss_feeds.title as feed, ttrss_feed_categories.title as category, COUNT(ref_id) as items, 
-							COUNT(NULLIF(marked, false)) as starred, COUNT(NULLIF(published, false)) AS published, ROUND(CAST(COUNT(ref_id) AS DECIMAL)/{$interval},2) as items_day
+		$result = db_query("SELECT
+							ttrss_feeds.title AS feed,
+							ttrss_feed_categories.title AS category,
+							COUNT(NULLIF(last_read > '{$datestr}', false)) AS items,
+							COUNT(NULLIF(last_marked > '{$datestr}', false)) AS starred,
+							COUNT(NULLIF(last_published > '{$datestr}', false)) AS published,
+							ROUND(CAST(COUNT(NULLIF(last_read > '{$datestr}', false)) AS DECIMAL) / {$interval}, 2) AS items_day
 							FROM ttrss_feeds
-							LEFT JOIN ttrss_user_entries ON ttrss_feeds.id = ttrss_user_entries.feed_id AND ttrss_user_entries.last_read > '{$datestr}'
+							LEFT JOIN ttrss_user_entries ON ttrss_feeds.id = ttrss_user_entries.feed_id
 							LEFT JOIN ttrss_entries ON ttrss_user_entries.ref_id = ttrss_entries.id
-							LEFT JOIN ttrss_feed_categories ON ttrss_feeds.cat_id = ttrss_feed_categories.id							
+							LEFT JOIN ttrss_feed_categories ON ttrss_feeds.cat_id = ttrss_feed_categories.id
 							WHERE ttrss_feeds.owner_uid = {$owner_uid}
 							GROUP BY feed, category
 							ORDER BY items_day DESC");
